3170a24cea3e1cdd53ec3f6ff3d02407d300e91b
Samuelisusername <115652284+Samuelisusername@users.noreply.github.com>
2024-02-21 09:46:10 +0100

    Update main.hs for additional understanding of what is going on

--- main.hs
+++ main.hs
@@ -6,7 +6,30 @@ main :: IO ()
 main = do
     someFunc
     let result = (hello 2 3)
-    putStrLn  ("Result of hello function: " ++ show result)
+    let result2 = (function 1 2)
+    putStrLn  ("Result of hello function: " ++ show result ++ show result2)
+function x y = x + y
+
+  --num1 <- read (getLine) :: Integer
+  --num2 <- read (getLine) :: Integer
+  --let res = myGcd num1 num2
+ -- putStrLn (show (myGcd 4 2))
+ --putStrLn "hello"
+ --name <- getLine
+-- putStrLn ("hello, "++ name ++ "!")
+
+  
+  
+--myGcd :: Int -> Int -> Int
+--myGcd x y
+--  x == y    = x
+-- y > x     = myGcd y x
+-- otherwise = myGcd (x - y) y
+ 
+--absGcd x y = myGcd (abs x) (abs y)
+
+
+
 
 
 
